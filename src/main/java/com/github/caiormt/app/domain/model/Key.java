bdb6d2e916f1676d6d9eef680620dad56fcbe2b5
Caio Tokunaga <caiormt@gmail.com>
2024-10-19 22:50:38 -0400

    feat(key): Add key model

---
+++ src/main/java/com/github/caiormt/app/domain/model/Key.java
@@ -0,0 +1,38 @@
+package com.github.caiormt.app.domain.model;
+
+import java.util.Objects;
+
+import org.apache.commons.lang3.StringUtils;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+import lombok.experimental.Accessors;
+
+@Getter
+@ToString
+@EqualsAndHashCode
+@Accessors(fluent = true)
+public final class Key {
+
+  private static final int MAX_LENGTH = 77;
+
+  private final Type type;
+  private final String value;
+
+  public enum Type {
+    CPF, CNPJ, PHONE, EMAIL, EVP
+  }
+
+  public Key(final Type type, final String value) {
+    if (Objects.isNull(type))
+      throw new RuntimeException("Key Type cannot be null");
+    if (Objects.isNull(value))
+      throw new RuntimeException("Key Value cannot be null");
+    if (StringUtils.length(value) > MAX_LENGTH)
+      throw new RuntimeException("Key Value length must be less than or equal 77");
+
+    this.type = type;
+    this.value = value;
+  }
+}
