bc68eec8d6217804c4c167b93207aa9de27fc5c9
Caio Tokunaga <caiormt@gmail.com>
2024-10-20 21:59:07 -0400

    feat(key): Add key validation

--- src/main/java/com/github/caiormt/app/domain/model/Key.java
+++ src/main/java/com/github/caiormt/app/domain/model/Key.java
@@ -1,6 +1,7 @@
 package com.github.caiormt.app.domain.model;
 
 import java.util.Objects;
+import java.util.regex.Pattern;
 
 import org.apache.commons.lang3.StringUtils;
 
@@ -19,6 +20,17 @@ public final class Key {
 
   private static final int MAX_LENGTH = 77;
 
+  private static final Pattern CPF_PATTERN = Pattern.compile(
+      "^[0-9]{11}$");
+  private static final Pattern CNPJ_PATTERN = Pattern.compile(
+      "^[0-9]{14}$");
+  private static final Pattern PHONE_PATTERN = Pattern.compile(
+      "^\\+[1-9]\\d{1,14}$");
+  private static final Pattern EMAIL_PATTERN = Pattern.compile(
+      "^[a-z0-9.!#$&'*+/=?^_`{|}~-]+@[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?(?:\\.[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?)*$");
+  private static final Pattern EVP_PATTERN = Pattern.compile(
+      "^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$");
+
   private final Type type;
   private final String value;
 
@@ -33,6 +45,16 @@ public final class Key {
       throw new KeyError("Key Value cannot be null");
     if (StringUtils.length(value) > MAX_LENGTH)
       throw new KeyError("Key Value length must be less than or equal 77");
+    if (Type.CPF == type && !CPF_PATTERN.matcher(value).matches())
+      throw new KeyError("Key Value must be a valid CPF");
+    if (Type.CNPJ == type && !CNPJ_PATTERN.matcher(value).matches())
+      throw new KeyError("Key Value must be a valid CNPJ");
+    if (Type.PHONE == type && !PHONE_PATTERN.matcher(value).matches())
+      throw new KeyError("Key Value must be a valid phone number");
+    if (Type.EMAIL == type && !EMAIL_PATTERN.matcher(value).matches())
+      throw new KeyError("Key Value must be a valid e-mail");
+    if (Type.EVP == type && !EVP_PATTERN.matcher(value).matches())
+      throw new KeyError("Key Value must be a valid UUID");
 
     this.type = type;
     this.value = value;
